63369757d810bb895d4b08cb94822ebf7a23de6e
Shadow-AI <53759750+Shadow-AI@users.noreply.github.com>
2023-06-08 17:10:33 -0400

    revert to browserrouter

--- src/index.js
+++ src/index.js
@@ -6,15 +6,15 @@ import "bootstrap/dist/js/bootstrap.min.js";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {HashRouter} from "react-router-dom";
+import {BrowserRouter, HashRouter} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     // browser router is for complete url match,
     // hashrouter just takes the latter part for URLs
-    <HashRouter hashType={"noslash"}>
+    <BrowserRouter basename={"/"}>
         <App/>
-    </HashRouter>
+    </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
