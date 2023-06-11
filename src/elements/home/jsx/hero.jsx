003204fae06e9f0fe6a75619414870a4875a31c2
Shadow-AI <53759750+Shadow-AI@users.noreply.github.com>
2023-06-11 16:49:14 -0400

    updates on homepage as per xcel

--- src/elements/home/jsx/hero.jsx
+++ src/elements/home/jsx/hero.jsx
@@ -1,5 +1,6 @@
 import "../css/hero.css";
 import {NavLink} from "react-router-dom";
+
 export default function Hero() {
     return (
         <div className={"hero"}>
@@ -8,16 +9,19 @@ export default function Hero() {
                     <div className="col d-flex justify-content-start align-items-center hero-parent">
                         <div className={"bg-img"}></div>
 
-                        <div className={"col-8 col-md-6 offset-2"}>
-                            <p className="text-justify">
-                                <b>You won't plan a trip without our recommendations again</b>
-                            </p>
-                            <p className="text-justify">
-                                Discover unique points of interest and personalised activities in just 2 minutes. All for free
-                            </p>
-                            <p className="d-flex justify-content-start pt-4">
-                                <GetStarted/>
-                            </p>
+                        <div className={"container"}>
+                            <div className={"col-8 col-md-6"}>
+                                <p className="text-justify">
+                                    <b>You won't plan a trip without our recommendations again</b>
+                                </p>
+                                <p className="text-justify">
+                                    Discover unique points of interest and personalised activities in just 2 minutes.
+                                    All for free
+                                </p>
+                                <p className="d-flex justify-content-start pt-4">
+                                    <GetStarted/>
+                                </p>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -29,7 +33,8 @@ export default function Hero() {
 
 function GetStarted() {
     return (
-        <NavLink to={"/recommendations"} id={"GetStarted"} className={"btn btn-outline-success"}>Personalise your Travel</NavLink>
+        <NavLink to={"/recommendations"} id={"GetStarted"} className={"btn btn-outline-success"}>Personalise your Trip
+            Today</NavLink>
         //equivalent to the below but for react router need to use Link
         // <a href="#" id="GetStarted" className="btn btn-outline-success">Personalise your Travel</a>
     );
--- src/elements/login/jsx/login.jsx
+++ src/elements/login/jsx/login.jsx
@@ -1,19 +1,28 @@
 import {NavLink} from "react-router-dom";
 import {SocialIcon} from "react-social-icons";
 import "../css/login.css";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faEye, faEyeSlash} from "@fortawesome/free-solid-svg-icons";
+import {useEffect, useState} from "react";
 
 function Login() {
     let val =
         <div className={"modal fade"} tabIndex={-1} id={"loginModal"}>
             <div className={"modal-dialog modal-dialog-centered"}>
                 <div className={"modal-content"}>
-                    <div className={"modal-header text-center d-flex justify-content-center"}>
+                    <div className={"modal-header d-flex justify-content-center position-relative"}>
                         <h3 className={"modal-title"}>
                             myTABA logo
                         </h3>
                         <button type={"button"} className={"btn-close"} data-bs-dismiss={"modal"}></button>
                     </div>
                     <div className={"modal-body"}>
+                        <p className={"text-center"}>
+                            Welcome to <b>myTABA</b>
+                        </p>
+                        <p className={"text-center"}>
+                            Travel planning, personalized for you
+                        </p>
                         <LoginBody/>
                     </div>
                 </div>
@@ -26,12 +35,56 @@ function Login() {
 }
 
 function LoginBody() {
+
+    let [passvisible, setpassvisible] = useState(false);
+    useEffect(() => {
+        const butt = document.getElementById("show-pass");
+
+        function toggleVisible() {
+            setpassvisible(!passvisible);
+        }
+
+        butt.addEventListener("click", toggleVisible);
+        return () => {
+            butt.removeEventListener("click", toggleVisible);
+        };
+    }, [passvisible]);
+
+    const [passVal, setPassVal] = useState('');
+    const [emailVal, setEmailVal] = useState('');
+    const handleUpdatePass = event => {
+        setPassVal(event.target.value);
+    }
+    const handleUpdateEmail = event => {
+        setEmailVal(event.target.value);
+    }
+
+    // dummy login tester
+    const handleSubmit = (e) => {
+        const email = "deepanshu@mytaba.com";
+        const password = "uwu";
+        if (!(email === emailVal && password === passVal)) {
+            e.preventDefault();
+            alert("Unsuccessful Login");
+        } else {
+            alert("login success");
+        }
+    };
+
     let val =
         <div className={"container"}>
-            <form className={"container"}>
+            <form className={"container"} onSubmit={handleSubmit}>
                 <div className={"form-fields"}>
-                    <input type={"email"} placeholder={"Email"} className={"form-control"} name={"email"}/>
-                    <input type={"password"} placeholder={"Password"} className={"form-control"} name={"email"}/>
+                    <input type={"email"} placeholder={"Email"} className={"form-control"} name={"email"}
+                           value={emailVal} onChange={handleUpdateEmail}/>
+                    <div className={"position-relative"}>
+                        <input type={passvisible ? "text" : "password"} placeholder={"Password"}
+                               className={"form-control"} name={"password"}
+                               value={passVal} onChange={handleUpdatePass}/>
+                        <button type={"button"} id={"show-pass"}>
+                            <FontAwesomeIcon icon={passvisible ? faEye : faEyeSlash}/>
+                        </button>
+                    </div>
                     <NavLink to={"#"} className={"container form-text"}>
                         Forgot password
                     </NavLink>
