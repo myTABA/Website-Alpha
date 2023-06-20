b3e3327fe93833173205d042d5aeb5f18a4b2c18
Shadow-AI <53759750+Shadow-AI@users.noreply.github.com>
2023-06-20 13:24:20 -0400

    streamlined travelguides with a gen function

--- src/elements/home/jsx/travelguide.jsx
+++ src/elements/home/jsx/travelguide.jsx
@@ -4,6 +4,24 @@ import {faBook} from "@fortawesome/free-solid-svg-icons";
 import {NavLink} from "react-router-dom";
 
 function TravelGuide() {
+    // this is the data
+    const cards = [
+        {
+            img: "https://images.unsplash.com/photo-1481627834876-b7833e8f5570?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8Ym9va3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=900&q=60"
+        },
+        {
+            img: "https://images.unsplash.com/photo-1501555088652-021faa106b9b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fG91dGRvb3J8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=900&q=60"
+        },
+        {
+            img: "https://images.unsplash.com/photo-1470246973918-29a93221c455?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fG91dGRvb3J8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=900&q=60"
+        },
+        {
+            img: "https://plus.unsplash.com/premium_photo-1677151193419-9be7a26c02cb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1760&q=80"
+        },
+        {
+
+        }
+    ];
     let val =
         <div className={"container"}>
             <div className={"row my-2"}>
@@ -12,26 +30,8 @@ function TravelGuide() {
                 </div>
             </div>
             <div className={"row"}>
-                <div className={"col-6 col-md-4 col-lg-3 p-2 m-0"}>
-                    <TravelCard
-                        img={"https://images.unsplash.com/photo-1481627834876-b7833e8f5570?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8Ym9va3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=900&q=60"}/>
-                </div>
-                <div className={"col-6 col-md-4 col-lg-3 p-2 m-0"}>
-                    <TravelCard
-                        img={"https://images.unsplash.com/photo-1501555088652-021faa106b9b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fG91dGRvb3J8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=900&q=60"}/>
-                </div>
-                <div className={"col-6 col-md-4 col-lg-3 p-2 m-0"}>
-                    <TravelCard
-                        img={"https://images.unsplash.com/photo-1470246973918-29a93221c455?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fG91dGRvb3J8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=900&q=60"}/>
-                </div>
-                <div className={"col-6 col-md-4 col-lg-3 p-2 m-0"}>
-                    <TravelCard
-                        img={"https://plus.unsplash.com/premium_photo-1677151193419-9be7a26c02cb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1760&q=80"}/>
-                </div>
-                <div className={"col-6 col-md-4 col-lg-3 p-2 m-0"}>
-                    <TravelCard/>
-                </div>
-                <div className={"col-6 col-md-4 col-lg-3 p-2 m-0"}>
+                <TravelCards cards={cards}/>
+                <div className={"col-12 col-md-4 col-lg-3 p-2 m-0"}>
                     <NavLink to={"/travel-guides"} className={"travel-link"}>
                         <div className={"card travel"}>
                             <div
@@ -55,6 +55,22 @@ function TravelGuide() {
         </div>
     );
 
+}
+
+// generator function
+function TravelCards({cards}){
+    let val = [];
+    for (const [i,card] of cards.entries()) {
+        val.push(
+            <div className={"col-12 col-md-4 col-lg-3 p-2 m-0"}>
+                <TravelCard
+                img={card.img}
+                location={card.location}
+                key={i+1}/>
+            </div>
+        );
+    }
+    return val;
 }
 
 function TravelCard({location, img}) {
--- src/elements/travelguide/jsx/travel.jsx
+++ src/elements/travelguide/jsx/travel.jsx
@@ -2,6 +2,21 @@ import {TravelCard} from "../../home/jsx/travelguide";
 import "../../home/css/travelguide.css";
 
 export default function Travel() {
+    const cards = [
+        {
+            img: "https://images.unsplash.com/photo-1481627834876-b7833e8f5570?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8Ym9va3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=900&q=60"
+        },
+        {
+            img: "https://images.unsplash.com/photo-1501555088652-021faa106b9b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fG91dGRvb3J8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=900&q=60"
+        },
+        {
+            img: "https://images.unsplash.com/photo-1470246973918-29a93221c455?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fG91dGRvb3J8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=900&q=60"
+        },
+        {
+            img: "https://plus.unsplash.com/premium_photo-1677151193419-9be7a26c02cb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1760&q=80"
+        },
+        {}
+    ];
     let val =
         <div className={"container"}>
             <div className={"row my-2"}>
@@ -10,29 +25,23 @@ export default function Travel() {
                 </div>
             </div>
             <div className={"row"}>
-                <div className={"col-6 col-md-4 col-lg-3 p-2 m-0"}>
-                    <TravelCard
-                        img={"https://images.unsplash.com/photo-1481627834876-b7833e8f5570?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8Ym9va3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=900&q=60"}/>
-                </div>
-                <div className={"col-6 col-md-4 col-lg-3 p-2 m-0"}>
-                    <TravelCard
-                        img={"https://images.unsplash.com/photo-1501555088652-021faa106b9b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fG91dGRvb3J8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=900&q=60"}/>
-                </div>
-                <div className={"col-6 col-md-4 col-lg-3 p-2 m-0"}>
-                    <TravelCard
-                        img={"https://images.unsplash.com/photo-1470246973918-29a93221c455?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fG91dGRvb3J8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=900&q=60"}/>
-                </div>
-                <div className={"col-6 col-md-4 col-lg-3 p-2 m-0"}>
-                    <TravelCard
-                        img={"https://plus.unsplash.com/premium_photo-1677151193419-9be7a26c02cb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1760&q=80"}/>
-                </div>
-                <div className={"col-6 col-md-4 col-lg-3 p-2 m-0"}>
-                    <TravelCard/>
-                </div>
-                <div className={"col-6 col-md-4 col-lg-3 p-2 m-0"}>
-                    <TravelCard/>
-                </div>
+                <TravelGenerator cards={cards}/>
             </div>
         </div>;
     return <div className={"travelGuide"}>{val}</div>;
+}
+
+const TravelGenerator = ({cards}) => {
+    let val = [];
+    for (const [i, card] of cards.entries()) {
+        val.push(
+            <div className={"col-12 col-md-4 col-lg-3 p-2 m-0"}>
+                <TravelCard
+                    img={card.img}
+                    location={card.location}
+                    key={i + 1}/>
+            </div>
+        );
+    }
+    return val;
 }
