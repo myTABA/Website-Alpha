69d4bd5163f3affbc8ffe8064e14232d8779ef4a
Shadow-AI <53759750+Shadow-AI@users.noreply.github.com>
2023-06-28 14:17:36 +0530

    website tracker changes

--- src/elements/home/home.jsx
+++ src/elements/home/home.jsx
@@ -8,7 +8,7 @@ import {useEffect} from "react";
 // needdd to do this because cant render custom text otherwise
 const TextFunction = ({text, pTag}) => {
     if (pTag) {
-        return <p>{text}</p>;
+        return <p className={"b2"}>{text}</p>;
     } else {
         return <>{text}<br/></>
     }
