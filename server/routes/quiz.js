1885e67c29d8faa671f25208d8412b62c0591f50
Joyce Ho <joyce880120@gmail.com>
2023-07-20 13:32:07 -0700

    connect to Rob's api call

--- server/routes/quiz.js
+++ server/routes/quiz.js
@@ -1,57 +1,143 @@
-const apiKey = '5ae2e3f221c38a28845f05b69faad9345b8deb30267f8430eabc0069';
+const apiKey = 'kjsdhf873thajgdgjs73gebsy8va2pft';
 const express = require('express');
 const router = express.Router();
 const axios = require('axios');
+const { S3Client, GetObjectCommand } = require('@aws-sdk/client-s3');
+const { getSignedUrl } = require('@aws-sdk/s3-request-presigner');
 
-// Array to store user selected location
-let location = [];
-// Array to store user selected types
-let types = [];
 let interest_level = 0;
-// Location examples
-// const recommendations = [
-//     { id: 1, name: 'Location1' },
-//     { id: 2, body: 'Location2' },
-//     { id: 3, name: 'Location3' },
-//     { id: 4, name: 'Location4' },
-//     { id: 5, name: 'Location5' }
-// ];
+let sessionId = 0;
+
+// Connect to s3
+const s3Client = new S3Client({
+    region: 'us-east-2', // Replace with your desired AWS region
+    credentials: {
+        accessKeyId: 'AKIA3MQ4Q2A7N475U46X',
+        secretAccessKey: 'droYSBJnsFX0WY/ElOaYN2g7mbMLyG+AzI0ivm1I',
+    }
+});
+async function generateSignedUrl (bucketName, objectKey) {
+    const command = new GetObjectCommand({
+        Bucket: bucketName,
+        Key: objectKey
+    });
+    try {
+        const signedUrl = await getSignedUrl(s3Client, command);
+        return signedUrl;
+    } catch (error) {
+        console.error('Error generating presigned URL:', error);
+        return null;
+    }
+}
 
 // Route for handling location_button
-router.post('/location-action', (req, res) => {
-    location = [];
-    const selectedCard = req.body.location;
-    if (!selectedCard) {
-        return res.status(400).json({ error: 'No place selected' })
+router.post('/location-action', async (req, res) => {
+    // const selectedCard = req.body.location;
+    selectedCard = 'barcelona_esp';
+    try {
+        const response = await axios.get(`https://xwim6jhv6d3rhgdpudpl3gusky0oycqp.lambda-url.us-east-2.on.aws/profiler/get-cluster/${selectedCard}?api-key=${apiKey}`);
+        const data = response.data;
+        // TODO : change S3 link to url for images
+        let clusters = data.cluster;
+        const promises = clusters.map((cluster) => {
+            let bucketName;
+            let objectKey;
+            imageURL = cluster.img;
+            if (imageURL !== "Joyce/Deepanchu! Send me the pictures and I'll put an addess to S3 HERE!") {
+                const parts = imageURL.split('/');
+                bucketName = parts[2]; // The bucket is the third element after splitting
+                objectKey = parts.slice(3).join('/');
+            } else {
+                // TODO: Hard code no S3 link photo
+                bucketName = 'mytaba-imgs';
+                objectKey = 'esp/barcelona/b25dcbf5609eb65a/f63f4d22278261cdca071c6b2daa5ab9.jpg';
+            }
+            
+            return generateSignedUrl(bucketName, objectKey)
+                .then((presignedURL) => {
+                if (presignedURL) {
+                    console.log('Presigned URL:', presignedURL);
+                    cluster.imagesURL = presignedURL;
+                }
+                })
+                .catch((error) => {
+                    res.status(500).json({ error: error.message });
+                });
+        });
+        // Wait for all promises to resolve before sending the response
+        Promise.all(promises)
+        .then(() => {
+            res.json(data);
+        })
+        .catch((error) => {
+            res.status(500).json({ error: error.message });
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'An error occurred' });
     }
-    location.push(selectedCard);
-    console.log(location);
-    res.status(200).json({selectedCard});
 });
  
 // Route for handling types_button
 router.post('/type-action', (req, res) => {
-    types = [];
-    const selectedCard = req.body.type;
-    if (!selectedCard) {
-        return res.status(400).json({ error: 'No type selected' })
-    }
-    types.push(selectedCard);
-    console.log(types);
-    res.status(200).json({selectedCard});
+    const apiUrl = `https://xwim6jhv6d3rhgdpudpl3gusky0oycqp.lambda-url.us-east-2.on.aws/profiler/initiate-session/?api-key=${apiKey}`;
+    // TODO: add selected poiids
+    // const data = req.body;
+    const data = {
+        "poiids": [
+          {
+            "poiid": "8602f63570f7ae9b",
+            "score": "1"
+          }
+        ],
+        "travelerid": "abcdefghijklmnop"
+      };
+    axios.put(apiUrl, data)
+        .then(response => {
+            console.log('PUT request successful');
+            console.log('Response:', response.data);
+            sessionId = response.data;
+        })
+        .catch(error => {
+            console.error('Error making PUT request:', error);
+        });
 });
 
 // Get first recommendation from API call
 router.get('/type-action', async (req, res) => {
-    const location_query = location[0];
-    const types_query = types;
+    // TODO : hard code session ID
+    sessionId = 'jsurn48fk2h8p0s1';
+    let poiId = 0;
     try {
-        // Make the API call to OpenTripMap
-        const response = await axios.get(`http://api.opentripmap.com/0.1/en/places/bbox?lon_min=2.1164359207060954&lat_min=41.3576824375304&lon_max=2.2404671435130012&lat_max=41.42204319076333&kinds=churches&limit=1&format=geojson&apikey=${apiKey}`);
-        // const response = await axios.get(`http://api.opentripmap.com/0.1/en/places/geoname?name=${location_query}&apikey=${apiKey}`);
-        const data = response.data;
-        // Process the data or send it back to the client
-        res.json(data);
+        const response = await axios.get(`https://3aphhcfwtbfk5wdqewyxbyakje0hpkfw.lambda-url.us-east-2.on.aws/recommender/session/${sessionId}?country=esp&city=barcelona&limit=1&topp=0.8&topk=200&temp=0.2&api-key=${apiKey}`);
+        data = response.data;
+        poiId = data.pois[0].poiid;
+        // Call get poi detail
+        const responseDetail = await axios.get(`https://bvln6ak7ovklghu2suidzwjrq40jhdju.lambda-url.us-east-2.on.aws/poidetails/get-poi-details/${poiId}?infolimit=name&infolimit=city&infolimit=country&infolimit=descrh&infolimit=descrc&infolimit=latlong&infolimit=ids&infolimit=images&api-key=${apiKey}`);
+        detail = responseDetail.data;
+        imageURL = detail.pois[0].images[0];
+        let bucketName;
+        let objectKey;
+        if (imageURL !== undefined) {
+            const parts = imageURL.split('/');
+            bucketName = parts[2]; // The bucket is the third element after splitting
+            objectKey = parts.slice(3).join('/');
+        } else {
+            // TODO : Hard code no S3 link photo
+            bucketName = 'mytaba-imgs';
+            objectKey = 'esp/barcelona/b25dcbf5609eb65a/f63f4d22278261cdca071c6b2daa5ab9.jpg';
+        }
+        generateSignedUrl(bucketName, objectKey)
+        .then((presignedURL) => {
+            if (presignedURL) {
+                console.log('Presigned URL:', presignedURL);
+                detail.pois[0].imagesURL = presignedURL;
+                res.json(detail);
+            }
+        })
+        .catch((error) => {
+            res.status(500).json({ error: error.message });
+        });
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'An error occurred' });
@@ -60,6 +146,7 @@ router.get('/type-action', async (req, res) => {
 
 // Route for handle interest_level button
 router.post('/interest-level-action/:id', (req, res) => {
+    // TODO : Update session id
     interest_level = 0;
     const interestResponse = req.body.interest;
     if (req.params.id >= '1' && req.params.id <= '5') {
@@ -72,26 +159,90 @@ router.post('/interest-level-action/:id', (req, res) => {
 
 // Route for getting update POI according to interest level
 router.get('/interest-level-action/:id', async (req, res) => {
+    sessionId = 'jsurn48fk2h8p0s1';
+    let poiId = 0;
     if (req.params.id >= '1' && req.params.id <= '4') {
         try {
-            // Make the API call to OpenTripMap
-            const response = await axios.get(`http://api.opentripmap.com/0.1/en/places/bbox?lon_min=2.1164359207060954&lat_min=41.3576824375304&lon_max=2.2404671435130012&lat_max=41.42204319076333&kinds=churches&limit=1&format=geojson&apikey=${apiKey}`);
-            // const response = await axios.get(`http://api.opentripmap.com/0.1/en/places/geoname?name=${location_query}&apikey=${apiKey}`);
-            const data = response.data;
-            // Process the data or send it back to the client
-            res.json(data);
+            const response = await axios.get(`https://3aphhcfwtbfk5wdqewyxbyakje0hpkfw.lambda-url.us-east-2.on.aws/recommender/session/${sessionId}?country=esp&city=barcelona&limit=1&topp=0.8&topk=200&temp=0.2&api-key=${apiKey}`);
+            data = response.data;
+            poiId = data.pois[0].poiid;
+            // Call get poi detail
+            console.log(poiId);
+            const responseDetail = await axios.get(`https://bvln6ak7ovklghu2suidzwjrq40jhdju.lambda-url.us-east-2.on.aws/poidetails/get-poi-details/${poiId}?infolimit=name&infolimit=city&infolimit=country&infolimit=descrh&infolimit=descrc&infolimit=latlong&infolimit=ids&infolimit=images&api-key=${apiKey}`);
+            // TODO : change S3 link to url for images
+            detail = responseDetail.data;
+            imageURL = detail.pois[0].images[0];
+            let bucketName;
+            let objectKey;
+            if (imageURL !== undefined) {
+                const parts = imageURL.split('/');
+                bucketName = parts[2]; // The bucket is the third element after splitting
+                objectKey = parts.slice(3).join('/');
+            } else {
+                // TODO : Hard code no S3 link photo
+                bucketName = 'mytaba-imgs';
+                objectKey = 'esp/barcelona/b25dcbf5609eb65a/f63f4d22278261cdca071c6b2daa5ab9.jpg';
+            }
+            generateSignedUrl(bucketName, objectKey)
+            .then((presignedURL) => {
+                if (presignedURL) {
+                    console.log('Presigned URL:', presignedURL);
+                    detail.pois[0].imagesURL = presignedURL;
+                    res.json(detail);
+                }
+            })
+            .catch((error) => {
+                res.status(500).json({ error: error.message });
+            });
         } catch (error) {
             console.error(error);
             res.status(500).json({ error: 'An error occurred' });
         }
     } else if (req.params.id === '5') {
         try {
-            // Make the API call to OpenTripMap
-            const response = await axios.get(`http://api.opentripmap.com/0.1/en/places/bbox?lon_min=2.1164359207060954&lat_min=41.3576824375304&lon_max=2.2404671435130012&lat_max=41.42204319076333&kinds=churches&limit=5&format=geojson&apikey=${apiKey}`);
-            // const response = await axios.get(`http://api.opentripmap.com/0.1/en/places/geoname?name=${location_query}&apikey=${apiKey}`);
-            const data = response.data;
-            // Process the data or send it back to the client
-            res.json(data);
+            const response = await axios.get(`https://3aphhcfwtbfk5wdqewyxbyakje0hpkfw.lambda-url.us-east-2.on.aws/recommender/session/${sessionId}?country=esp&city=barcelona&limit=10&topp=0.8&topk=200&temp=0.2&api-key=${apiKey}`);
+            data = response.data;
+            // Call get poi detail
+            const jsonData = [];
+            let elements = data.pois;
+            const promises = elements.map(async(element) => {
+                poiId = element.poiid;
+                const responseDetail = await axios.get(`https://bvln6ak7ovklghu2suidzwjrq40jhdju.lambda-url.us-east-2.on.aws/poidetails/get-poi-details/${poiId}?infolimit=name&infolimit=city&infolimit=country&infolimit=descrh&infolimit=descrc&infolimit=latlong&infolimit=ids&infolimit=images&api-key=${apiKey}`);
+                element.poiDetail = responseDetail.data.pois[0];
+                // TODO : change S3 link to url for images
+                imageURL = element.poiDetail.images[0];
+                let bucketName;
+                let objectKey;
+                if (imageURL !== undefined) {
+                    const parts = imageURL.split('/');
+                    bucketName = parts[2]; // The bucket is the third element after splitting
+                    objectKey = parts.slice(3).join('/');
+                } else {
+                    // TODO : Hard code no S3 link photo
+                    bucketName = 'mytaba-imgs';
+                    objectKey = 'esp/barcelona/b25dcbf5609eb65a/f63f4d22278261cdca071c6b2daa5ab9.jpg';
+                }
+                
+                return generateSignedUrl(bucketName, objectKey)
+                    .then((presignedURL) => {
+                        if (presignedURL) {
+                            console.log('Presigned URL:', presignedURL);
+                            element.poiDetail.imagesURL = presignedURL;
+                            jsonData.push(element);
+                        }
+                    })
+                    .catch((error) => {
+                        res.status(500).json({ error: error.message });
+                    });
+            });
+            // Wait for all promises to resolve before sending the response
+            Promise.all(promises)
+            .then(() => {
+                res.json(jsonData);
+            })
+            .catch((error) => {
+                res.status(500).json({ error: error.message });
+            });
         } catch (error) {
             console.error(error);
             res.status(500).json({ error: 'An error occurred' });
